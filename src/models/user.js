ca8e144d1ddd7c9f9fa9b8da9afd404cea5ac8ea
Sheetal <105560345+sheetal2604@users.noreply.github.com>
2026-07-05 22:09:15 +0530

    added timestamp also in user schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,53 +1,58 @@
 const mongoose = require("mongoose");
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true,
-    minLength: 3,
-    maxLength: 30,
-    trim: true,
-  },
-  lastName: {
-    type: String,
-    minLength: 3,
-    maxLength: 30,
-    trim: true,
-  },
-  emailId: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    validate(value) {
-      if (value.indexOf("@") === -1)
-        throw new Error("Email must contain '@' symbol");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 30,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      minLength: 3,
+      maxLength: 30,
+      trim: true,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      validate(value) {
+        if (value.indexOf("@") === -1)
+          throw new Error("Email must contain '@' symbol");
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["male", "female", "other"].includes(value))
+          throw new Error("Gender must be male, female or other");
+      },
+    },
+    about: {
+      type: String,
+      default: "Hey there! I am using DevTinder",
+      trim: true,
+    },
+    skills: {
+      type: [String],
     },
   },
-  password: {
-    type: String,
-    required: true,
-    trim: true,
+  {
+    timestamps: true,
   },
-  age: {
-    type: Number,
-    min: 18,
-  },
-  gender: {
-    type: String,
-    validate(value) {
-      if (!["male", "female", "other"].includes(value))
-        throw new Error("Gender must be male, female or other");
-    },
-  },
-  about: {
-    type: String,
-    default: "Hey there! I am using DevTinder",
-    trim: true,
-  },
-  skills: {
-    type: [String],
-  },
-});
+);
 
 module.exports = mongoose.model("User", userSchema);
